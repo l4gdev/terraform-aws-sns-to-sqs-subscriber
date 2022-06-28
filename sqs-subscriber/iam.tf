a9474735e8f645ca1e25b3eeb3b6cda3f74a337c
Arnold Sobanski <arnold.sobanski@l4g.eu>
2022-06-28 17:00:28 +0100

    add iam perms for resource

--- sqs-subscriber/iam.tf
+++ sqs-subscriber/iam.tf
@@ -1,4 +1,21 @@
-#resource "aws_iam_role_policy" "allow_subscribe_sss" {
-#  policy = ""
-#  role   = ""
-#}
+resource "aws_iam_role_policy" "test_policy" {
+  name   = "allow-access-to-${aws_sqs_queue.sqs.name}"
+  role   = var.application_iam_role_arn
+  policy = data.aws_iam_policy_document.allow.json
+}
+
+
+
+data "aws_iam_policy_document" "allow" {
+  sid = "1"
+
+  actions = [
+    "sqs:ListQueues",
+    "sqs:GetQueueUrl",
+    "sqs:ListDeadLetterSourceQueues",
+    "sqs:ReceiveMessage",
+    "sqs:GetQueueAttributes",
+    "sqs:ListQueueTags"
+  ]
+  resources = [aws_sqs_queue.sqs.arn]
+}
