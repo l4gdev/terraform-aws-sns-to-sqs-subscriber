ee40a2cf8545d41811500cad05846f3315383e40
Arnold Sobanski <arnold.sobanski@l4g.eu>
2022-06-30 22:00:29 +0100

    tf update

---
+++ sqs-subscriber-iam/iam.tf
@@ -0,0 +1,33 @@
+variable "application_iam_role_name" {
+  type = string
+}
+resource "aws_iam_role_policy" "sqs" {
+  name   = "${replace(var.sqs_name,".","-")}"
+  role   = var.application_iam_role_name
+  policy = data.aws_iam_policy_document.allow.json
+}
+
+data "aws_iam_policy_document" "allow" {
+  statement {
+    sid = "1"
+
+    actions = [
+      "sqs:ListQueues",
+      "sqs:GetQueueUrl",
+      "sqs:ListDeadLetterSourceQueues",
+      "sqs:ReceiveMessage",
+      "sqs:GetQueueAttributes",
+      "sqs:ListQueueTags"
+    ]
+    resources = [data.aws_sqs_queue.queue.arn]
+  }
+}
+
+
+variable "sqs_name" {
+  type = string
+}
+
+data "aws_sqs_queue" "queue" {
+  name = var.sqs_name
+}
--- sqs-subscriber/outputs.tf
+++ sqs-subscriber/outputs.tf
@@ -3,6 +3,11 @@ output "sns_arn" {
   value = var.sns_arn
 }
 
-output "sqs_arn" {
-  value = aws_sqs_queue.sqs.arn
+output "sqs_url" {
+  value = aws_sqs_queue.sqs.url
+}
+
+
+output "sqs_name" {
+  value = aws_sqs_queue.sqs.name
 }
